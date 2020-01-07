2e3b18100a08c3b74346b7b2160c343c894c5c2b
PearlFighter <ocx23@yahoo.com>
2020-01-07 11:01:19 -0500

    fixed redux issues

--- src/index.js
+++ src/index.js
@@ -7,40 +7,14 @@ import * as serviceWorker from './serviceWorker';
 
 
 import { Provider } from 'react-redux'
-import store from './store'
 
-import { connect } from 'react-redux'
-import { increment, decrement, reset } from './actionCreators'
+
 
 // const Counter = ...
 
-const mapStateToProps = (state /*, ownProps*/) => {
-  return {
-    counter: state.counter
-  }
-}
-
-const mapDispatchToProps = { increment, decrement, reset }
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Counter)
 
 
-
-
-const rootElement = document.getElementById('root')
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  rootElement
-)
-
-
-
-//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
