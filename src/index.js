89288c541f21d16a1325808cc8446fd6411dfbe5
PearlFighter <ocx23@yahoo.com>
2020-01-07 10:52:46 -0500

    code for react-redux

--- src/index.js
+++ src/index.js
@@ -4,9 +4,47 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+import { Provider } from 'react-redux'
+import store from './store'
+
+import { connect } from 'react-redux'
+import { increment, decrement, reset } from './actionCreators'
+
+// const Counter = ...
+
+const mapStateToProps = (state /*, ownProps*/) => {
+  return {
+    counter: state.counter
+  }
+}
+
+const mapDispatchToProps = { increment, decrement, reset }
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter)
+
+
+
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+)
+
+
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
